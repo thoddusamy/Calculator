2692fd5efd4da83861e25cd3362e24fab7bf8624
THODDUSAMY G <103572861+thoddusamy@users.noreply.github.com>
2022-06-05 09:25:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let headerEle = document.createElement('header')
 headerEle.innerHTML = `
-<h3>This is a basic calculator App. Use this calculator for preform with addition, subtraction, division
+<h3>This is a basic calculator App. Use this calculator to preform with addition, subtraction, division
 and multiplication and modulus.</h3>`
 
 let div = document.createElement('div')
